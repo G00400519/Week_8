302c031c007b1f52cb67a814db235399ad09a53f
g00400519 <g000400519@atu.ie>
2022-11-15 14:03:13 +0000

    Sending file week_8

--- src/main/java/iw/atu/week8/week8.java
+++ src/main/java/iw/atu/week8/week8.java
@@ -6,8 +6,18 @@ public class week8 {
     public static void main(String[] args) {
 
         {
-            File myFile = new File("MyFile.txt");
+            File myFile = new File("MyFile2.txt");
             System.out.println("My file is located at " + myFile.getAbsolutePath());
+            String content = "Details to write";
+                try
+                {
+                    FileWriter myWriter = new FileWriter(myFile, true);
+                    myWriter.write("how are you?");
+                    myWriter.close();
+                } catch (IOException e)
+                {
+                    e.printStackTrace();
+                }
         }
 
     }
