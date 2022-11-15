70359975befe673b2e59164de99568ee3c99e078
g00400519 <g000400519@atu.ie>
2022-11-15 13:40:54 +0000

    Sending file wee8

---
+++ src/main/java/iw/atu/week8/week8.java
@@ -0,0 +1,14 @@
+package iw.atu.week8;
+
+import java.io.* ;
+
+public class week8 {
+    public static void main(String[] args) {
+
+        {
+            File myFile = new File("MyFile.txt");
+            System.out.println("My file is located at " + myFile.getAbsolutePath());
+        }
+
+    }
+}
